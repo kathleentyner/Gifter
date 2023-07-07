bc6b597f4181b429fa9ff82f44a55db68d97fd8b
Kathleen Tyner <kathleentyner@gmail.com>
2023-07-07 15:27:37 -0400

    setting up initial files

---
+++ Gifter/Repositories/IPostRepository.cs
@@ -0,0 +1,15 @@
+﻿using Gifter.Models;
+
+namespace Gifter.Repositories
+{
+    public interface IPostRepository
+    {
+        List<Post> GetAll();
+        Post GetById(int id);
+        void Add(Post post);
+        void Update(Post post);
+        void Delete(int id);
+
+
+    }
+}
